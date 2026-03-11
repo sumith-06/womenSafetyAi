c5c0cf9915a019f1cba51ac17363f8fae2bb415c
sumith-06 <agirisumith06@gmail.com>
2026-03-11 23:07:25 +0530

    fixed leaflet maarker icon issue

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,6 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 function MapView() {
 
   const position = [17.3457, 78.5520]; // LB Nagar Hyderabad
